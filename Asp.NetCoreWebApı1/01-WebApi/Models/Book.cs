996090746dd0dc90160c119983f4ae8f649913cf
Sadık Sünbül <jsjsqwe12@gmail.com>
2023-07-08 05:52:10 +0300

    ilk komit

---
+++ Asp.NetCoreWebApı1/01-WebApi/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_WebApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+    }
+}
